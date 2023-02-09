4b06008acaee78c08d3c01db8161a566e264afec
vincent <tranhao491999@gmail.com>
2023-02-09 15:17:02 +0700

    first commit

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase-admin/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxylD8G0sKw31MmxaP6qZBZutZ6I9V_FM",
+  authDomain: "api-graphql-6f537.firebaseapp.com",
+  projectId: "api-graphql-6f537",
+  storageBucket: "api-graphql-6f537.appspot.com",
+  messagingSenderId: "642245686902",
+  appId: "1:642245686902:web:aacd1ef21290afbd1fd157",
+  measurementId: "G-EQ5BYW1E6F",
+};
+
+initializeApp(firebaseConfig);
